660411fd9bd9638c2702b4c5c0085d8849c7a846
Andrey Chirskiy <andrey_chirskiy@govitall.com>
2017-03-18 17:56:34 +0200

    added Facade to config

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Internal ServiceProvider
+         */
+        Chipk4\Selectel\SelectelApiServiceProvider::class,
+
     ],
 
     /*
@@ -225,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'SelectelApi' => Chipk4\Selectel\SelectelApiFacade::class,
     ],
 
 ];
