2f8c3aed142cfd928f2a2d17ef840ade31d24e83
Andrey Chirskiy <andrey_chirskiy@govitall.com>
2017-03-19 19:29:07 +0200

    added storageUrl and return view to config

--- packages/chipk4/selectel/config/selectel-api.php
+++ packages/chipk4/selectel/config/selectel-api.php
@@ -13,12 +13,25 @@ return [
     'authKey' => env('SELECTEL_PASSWORD'),
 
     /*
-     * Default API url
+     * API url
      */
     'apiUrl' => 'https://auth.selcdn.ru/',
 
     /*
      * Default value for request timeout
      */
-    'timeout' => 10
+    'timeout' => 10,
+
+    /*
+     * Default storage url
+     */
+    'storageUrl' => env('SELECTEL_STORAGE_URL'),
+
+    /*
+     * Response view
+     * Can be in json or xml
+     */
+    'returnView' => 'json',
+
+
 ];
